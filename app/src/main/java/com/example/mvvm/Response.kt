e798f07b06d01cf16898f2e96f89cd2baaff212a
unknown <yomna.ashraf559@gmail.com>
2025-03-23 22:35:40 +0200

    update

--- app/src/main/java/com/example/mvvm/Response.kt
+++ app/src/main/java/com/example/mvvm/Response.kt
@@ -3,7 +3,7 @@ package com.example.mvvm
 import com.example.mvvm.data.model.Product
 
 sealed class Response<out T> {
-    class Loading<T> : Response<T>() // ✅ Allow Loading to hold a generic type
+    class Loading<T> : Response<T>()
     data class Success<T>(val data: T) : Response<T>()
-    data class Failure<T>(val error: Throwable) : Response<T>() // ✅ Ensure Failure is generic
+    data class Failure<T>(val error: Throwable) : Response<T>()
 }
